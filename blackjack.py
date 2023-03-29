ee8c7bc36b43457e38c4096bb04cb4b121a7d3ff
Aadil Hussain <ault@andrims.com>
2023-03-29 14:52:25 +0000

    Fixed playerdata dict init and init deal assigns.

--- blackjack.py
+++ blackjack.py
@@ -2,30 +2,33 @@ import random
 
 players = {}
 
+
 def getNum(prompt: str):
     while True:
         try:
             num = int(input(prompt))
             break
-        except(ValueError):
+        except (ValueError):
             print("Invalid Input, please enter a number.")
     return num
 
+
 def displayCard(suit, value):
-    #depending on suit, makes a nice line on ascii card to fit suit name
+    # depending on suit, makes a nice line on ascii card to fit suit name
     if suit == 'hearts':
         line2 = '|-Hearts-|'
     elif suit == 'clubs':
-        pass #need to finish laterr
+        pass  # need to finish laterr
     elif suit == 'spades':
-        pass #need to finish laterr
+        pass  # need to finish laterr
     elif suit == 'diamonds':
-        pass #need to finish laterr
+        pass  # need to finish laterr
     print("""
 ----------
 |--------| 
-    """) # its 1:30am rn im broken inside, we work on this later lol
+    """)  # its 1:30am rn im broken inside, we work on this later lol
     # this is last priority function, can use text identifiers for now. ascii later ig
+
 
 def Init():
     global players
@@ -37,24 +40,25 @@ def Init():
 
     # find out how many players will be playing
     numPlayers = getNum("\nHow many players will be playing?: ")
-    
+
     for num in range(1, numPlayers+1):
         # create playerdata within players dict
         # include name, amount, and hands with hand values within nested dict
         players[num] = {
-            "name": input("What is the name for Player %i?: "%num),
+            "name": input("What is the name for Player %i?: " % num),
             "bank": 100,
-            "hands": {1:[]}
+            "hands": {1: [0]}
         }
-        print("Okay %s, welcome to the game!\n"%players[num]['name'])
+        print("Okay %s, welcome to the game!\n" % players[num]['name'])
     print("Alright, the players are as follows:")
     for item in players:
-        print("Player %i: %s"%(item, players[item]['name']))
+        print("Player %i: %s" % (item, players[item]['name']))
+
 
 def promptOptions(player, canDoubleDown):
     # prompts player to hit or stand
     while True:
-        print("%s, what do you want to do?"%player['name'])
+        print("%s, what do you want to do?" % player['name'])
         print('''
         Options:
         1. Hit
@@ -76,15 +80,17 @@ def promptOptions(player, canDoubleDown):
         else:
             print("Invalid option, try again.")
 
+
 def canPlayerDoubDown(playerhand):
-    if playerhand[1] == playerhand[2]:
+    if playerhand[0] == playerhand[1]:
         return True
     else:
         return False
 
+
 def dealCard():
-    #generates a random suit for the card dealt
-    shapeGen = random.randint(1,4)
+    # generates a random suit for the card dealt
+    shapeGen = random.randint(1, 4)
     if shapeGen == 1:
         shape = "hearts"
     elif shapeGen == 2:
@@ -97,8 +103,8 @@ def dealCard():
         print("Error in func:dealCard()\nerr:shapeGen issue")
         quit()
 
-    #generates a random number between 1 and 10 for the card's value and assigns it to 'value'
-    valueGen = random.randint(1,10)
+    # generates a random number between 1 and 10 for the card's value and assigns it to 'value'
+    valueGen = random.randint(1, 10)
     if valueGen == 1:
         # value = "ace"
         value = 10
@@ -111,20 +117,25 @@ def dealCard():
     else:
         print("Error in func:dealCard()\nerr:valueGen issue")
         quit()
-    
+
     return value, shape
+
 
 def playerHit(player, aceCheck):
     raise NotImplementedError
 
+
 def playerStand(player):
     raise NotImplementedError
+
 
 def playerDoubleDown(player):
     raise NotImplementedError
 
+
 def displayHand():
     raise NotImplementedError
+
 
 Init()
 
@@ -133,16 +144,17 @@ Init()
 while True:
     for player in players:
         # deals player a hand at start of round
-        for i in range(0,2):
+        for i in range(0, 2):
             value, shape = dealCard()
             if value == 1:
                 aceAvail = True
                 players[player]['hands'][1].append(value)
             else:
-                players[player]['hands'][1] += value
+                players[player]['hands'][1][0] += value #need to change to save cards in hand separately - to check if can doubdown
 
         # plays the players' hand
-        play = promptOptions(player, canPlayerDoubDown(players[player]['hands'][1]))
+        play = promptOptions(player, canPlayerDoubDown(
+            players[player]['hands'][1]))
         if play == 'hit':
             playerHit(player, aceAvail)
         elif play == 'stand':
@@ -152,12 +164,13 @@ while True:
         else:
             print("Error in main play loop\nerr:play not valid")
             quit()
-    
+
         # clear player hands after each turn
         players[player]['hands'][1].clear()
 
     # if any player wishes to quit the application between rounds, quit()
-    quitQuery = input("Would any player like to leave the game at this time?[yes/no]: ").upper()
+    quitQuery = input(
+        "Would any player like to leave the game at this time?[yes/no]: ").upper()
     if quitQuery[1] == "Y":
         quit()
     elif quitQuery[1] == "N":
