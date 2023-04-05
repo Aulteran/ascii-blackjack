3025157be1a89e4a596f51dcfe53adff540d5023
Aadil Hussain <ault@andrims.com>
2023-04-05 13:10:08 -0400

    Attempted hand eval + created testcase
    Co-Authored-By: divyd8314 <84455302+divyd8314@users.noreply.github.com>

--- blackjack-v0.2.py
+++ blackjack-v0.2.py
@@ -35,8 +35,7 @@ def deal_card():
     valueGen = random.randint(1, 10)
     # this IF-statement is unnecessary, but kept incase wanna rework face assignments.
     if valueGen == 1:
-        # value = "ace"
-        value = 10
+        value = 11
     elif valueGen == 10:
         # possibleFaces = ['king', 'queen', 'jack']
         # value = random.choice(possibleFaces)
@@ -51,21 +50,31 @@ def deal_card():
 
 
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, hands={}):
         self.name = name
         self.money = 100
-        self.hands = {}
+        self.hands = hands
         self.hand_doubled_down = bool
         self.hand_split = bool
 
-    def eval_hand():
+    def eval_hand(self, handIndex):
+        add = sum(self.hands[handIndex])
+        for card in self.hands[handIndex]:
+            print(card)
+            if card==11:
+                if add>21:
+                    card=1
+                    add = sum(self.hands[handIndex])
+                    continue
+                break
+        return sum(self.hands[handIndex])
+       
         # find how many aces in hand
         # find total hand val without aces accounted
         # check if handval is above 21
         # add first ace as 11, check if handval >21
         # if yes, add ace as 1, check if handval >21
         # if handval over 21, bust.
-        raise NotImplementedError
     
     def player_hit(player, handIndex):
         card_value, card_shape = deal_card()
@@ -93,40 +102,46 @@ players = []
 # create players
 for i in range(numPlayers):
     # Creates player object
-    player = Player(input('Enter name for Player {}: '.format(i+1)))
+    player = Player(input('Enter name for Player {}: '.format(i+1)), {0: [11, 11]})
     players.append(player)
 print() # spacer to separate roster readback
 
-# player roster readback/check
 for player in players:
-    print("Player {}: {} with ${}".format(players.index(player) + 1, player.name, player.money))
+    print(player.eval_hand(0))
 
-# still debating what to do with this, will come back to it later
-# readback_verification = lambda verif=input('Is this playerlist correct?[Y/N]: ').upper(): True if verif[0] == 'Y' else False if verif[0] == 'N' else print('invalid input')
+# # player roster readback/check
+# for player in players:
+#     print("Player {}: {} with ${}".format(players.index(player) + 1, player.name, player.money))
 
-while True:
-    # at start of round, display current stats, clear all hands, and deal new hand
-    print("\n=========================================\nCurrent Stats: ")
-    for player in players:
-        print("{} with ${}".format(player.name, player.money))
-        player.hand_doubled_down = False; player.hand_split = False; player.hands.clear()
-        card_value1, card_shape1 = deal_card()
-        card_value2, card_shape2 = deal_card()
-        player.hands[0] = [card_value1, card_value2]
-    print("=========================================\n")
+# # still debating what to do with this, will come back to it later
+# # readback_verification = lambda verif=input('Is this playerlist correct?[Y/N]: ').upper(): True if verif[0] == 'Y' else False if verif[0] == 'N' else print('invalid input')
 
-    # show each player their hand and ask for play
+# while True:
+#     # at start of round, display current stats, clear all hands, and deal new hand
+#     print("\n=========================================\nCurrent Stats: ")
+#     for player in players:
+#         print("{} with ${}".format(player.name, player.money))
+#         player.hand_doubled_down = False; player.hand_split = False; player.hands.clear()
+#         card_value1, card_shape1 = deal_card()
+#         card_value2, card_shape2 = deal_card()
+#         player.hands[0] = [card_value1, card_value2]
+#     print("=========================================\n")
 
-    # process all playerhands with dealerhand and check for blackjack
+#     # show each player their hand and ask for play
+#     for player in players:
+#         print(player.hands[0])
+#         print(player.eval_hand(0))
 
-    # too sleepy now, we make later :)
+#     # process all playerhands with dealerhand and check for blackjack
 
-    # if any player wishes to quit the application between rounds, quit()
-    quitQuery = input("Would any player like to leave the game at this time?[yes/no]: ").upper()
-    if quitQuery[1] == "Y":
-        print('Quitting the application:\nuser executed')
-        break
-    elif quitQuery[1] == "N":
-        continue # break this iteration of while loop and move on to next iteration.
-    else:
-        print("Error in main play loop\nerr:quitQuery not valid")
+#     # too sleepy now, we make later :)
+
+#     # if any player wishes to quit the application between rounds, quit()
+#     quitQuery = input("Would any player like to leave the game at this time?[yes/no]: ").upper()
+#     if quitQuery[1] == "Y":
+#         print('Quitting the application:\nuser executed')
+#         break
+#     elif quitQuery[1] == "N":
+#         continue # break this iteration of while loop and move on to next iteration.
+#     else:
+#         print("Error in main play loop\nerr:quitQuery not valid")
